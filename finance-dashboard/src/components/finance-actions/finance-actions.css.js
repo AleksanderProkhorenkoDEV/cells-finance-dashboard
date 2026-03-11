3b25a2a5f9f03f94e3a570b54b550c57d400e918
aleksander <aprokhorenkodev@gmail.com>
2026-03-11 15:29:54 +0100

    CELLS | STYLE | feat: add style to the component structure

--- finance-dashboard/src/components/finance-actions/finance-actions.css.js
+++ finance-dashboard/src/components/finance-actions/finance-actions.css.js
@@ -1,3 +1,21 @@
 import { css } from 'lit'
 
-export const styles = css``;
+export const styles = css`
+
+    :host{
+
+        display:flex;
+        flex-direction:column;
+        gap:1rem;
+
+        width:100%;
+        height:100%;
+    }
+
+    .action__title{
+        margin:0;
+
+        text-align:center;
+        text-transform:uppercase;
+    }
+`;
