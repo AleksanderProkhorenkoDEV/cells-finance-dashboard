c438e0014cbef60b5be1d1af540bc496ee819bb9
aleksander <aprokhorenkodev@gmail.com>
2026-03-11 15:33:59 +0100

    CELLS | STYLE | feat: add styles to action form

---
+++ finance-dashboard/src/components/form/actions-form.css.js
@@ -0,0 +1,22 @@
+import { css } from "lit";
+
+export const styles = css`
+
+    :host{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+
+        width:100%;
+        height:100%;
+    }
+
+    .form{
+        display:flex;
+        flex-direction:column;
+        gap:1rem;
+
+        width:65%;
+        height: 80%;
+    }
+`
