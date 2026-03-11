a1e8fda4a6cc604f9df17388395d82a55f1d6591
aleksander <aprokhorenkodev@gmail.com>
2026-03-11 13:35:52 +0100

    CELLS | COMPONENT | feat: create a custom input

---
+++ finance-dashboard/src/components/inputs/input.css.js
@@ -0,0 +1,24 @@
+import { css } from "lit";
+
+export const styles = css`
+    input {
+        border: none;
+        outline: none;
+        background: none;
+        font: inherit;
+        color: inherit;
+        box-sizing: border-box;
+        -webkit-appearance: none;
+        appearance: none;
+    }
+
+    .structural{
+
+        width:100%;
+
+        padding:.7rem 0.3rem;
+
+        border-radius:6px;
+        border: 1px solid var(--color-text-secondary)
+    }
+`
---
+++ finance-dashboard/src/components/inputs/input.ts
@@ -0,0 +1,32 @@
+import { ElementController } from "@open-cells/element-controller";
+import { customElement, property } from "lit/decorators.js";
+import { styles } from "./input.css.js";
+import { html, LitElement } from "lit";
+
+@customElement('wc-input')
+export class Input extends LitElement {
+    elementController = new ElementController(this)
+
+    @property({ type: String })
+    value: string = "";
+
+    @property({ attribute: false })
+    type: HTMLInputElement["type"] = "text"
+
+    @property({ type: Boolean })
+    required: boolean = true
+
+    static styles = styles
+
+
+    render() {
+        return html` 
+            <input 
+                class="structural"
+                .value=${this.value}
+                .type=${this.type}
+                .required=${this.required ? true : false}
+            />
+        `
+    }
+}
