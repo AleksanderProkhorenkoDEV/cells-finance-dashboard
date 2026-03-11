5b4d944da613bf9a8eae248eebc6043ae1a40b50
aleksander <aprokhorenkodev@gmail.com>
2026-03-11 13:37:22 +0100

    CELLS | STYLE | feat: add hidden property

--- finance-dashboard/src/components/cards/card.css.js
+++ finance-dashboard/src/components/cards/card.css.js
@@ -3,7 +3,9 @@ import { css } from "lit";
 export const styles = css`
     :host{
         border-radius:0.5rem;
-        border: 1px solid var(--color-border)
+        border: 1px solid var(--color-border);
+
+        overflow:hidden;
     }
     article{
         width:100%;
