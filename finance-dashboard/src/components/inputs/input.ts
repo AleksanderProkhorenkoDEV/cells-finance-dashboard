dd7c4c8abb069b31d3fc7adab0966c7b163ad756
aleksander <aprokhorenkodev@gmail.com>
2026-03-12 12:45:29 +0100

    CELLS | COMPONENT | feat: add UX property and finish the logic of input

--- finance-dashboard/src/components/inputs/input.ts
+++ finance-dashboard/src/components/inputs/input.ts
@@ -8,7 +8,13 @@ export class Input extends LitElement {
     elementController = new ElementController(this)
 
     @property({ type: String })
+    name: string = "";
+
+    @property({ type: String })
     value: string = "";
+
+    @property({ type: String })
+    placeholder: string = "";
 
     @property({ attribute: false })
     type: HTMLInputElement["type"] = "text"
@@ -18,14 +24,28 @@ export class Input extends LitElement {
 
     static styles = styles
 
+    private _onInput(e: Event) {
+        const target = e.target as HTMLInputElement;
+        this.value = target.value; 
+
+        this.dispatchEvent(new CustomEvent('input', {
+            detail: this.value,
+            bubbles: true,
+            composed: true
+        }));
+    }
+
 
     render() {
         return html` 
             <input 
                 class="input input__field"
+                name=${this.name}
+                .placeholder=${this.placeholder}
                 .value=${this.value}
                 .type=${this.type}
                 .required=${this.required ? true : false}
+                @input=${this._onInput}
             />
         `
     }
