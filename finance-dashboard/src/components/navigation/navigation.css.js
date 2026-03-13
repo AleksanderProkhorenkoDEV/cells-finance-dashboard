bf089541b1c7967c3db03b5829b5fc69f55eaead
aleksander <aprokhorenkodev@gmail.com>
2026-03-13 10:37:35 +0100

    CELLS | NAVIGATION | feat: create a main navigation menu

---
+++ finance-dashboard/src/components/navigation/navigation.css.js
@@ -0,0 +1,39 @@
+import { css } from "lit";
+
+export const styles = css`
+    .nav{
+        width:100%;
+        height:100;
+
+        display:flex;
+        justify-content:end;
+
+        border-bottom:1px solid var(--color-border);
+    }
+
+    .nav__host{
+        display:flex;
+        align-items:center;
+        gap:1.5rem;
+
+        margin-right:2rem;
+        padding:1rem;
+
+        list-style:none;
+    }
+
+    .nav__items{
+        text-transform:uppercase;
+        letter-spacing:2px;
+    }
+
+    .nav__items--active{
+
+        padding:0.4rem 0.6rem;
+
+        border-radius: 4px;
+        border:1px solid var(--color-primary-hover);
+
+        background-color: color-mix(var(--color-primary-hover), transparent 50%);
+    }
+`
---
+++ finance-dashboard/src/components/navigation/navigation.ts
@@ -0,0 +1,46 @@
+import { ElementController } from "@open-cells/element-controller";
+import { customElement, state } from "lit/decorators.js";
+import { styles } from "./navigation.css.js";
+import { html, LitElement } from "lit";
+
+@customElement('wc-navigation')
+export class Navigation extends LitElement {
+    elementController = new ElementController(this)
+
+    @state()
+    private _activePage = 'home';
+
+    static styles = styles
+
+
+    _handleNavigationClick = (e: Event) => {
+        const li = e.target as HTMLElement
+
+        const pageTogo = li.dataset.route as string;
+        this.elementController.navigate(pageTogo)
+        this._activePage = pageTogo;
+    }
+
+    render() {
+        const menuItems = ['home', 'balance', 'movements', 'summary'];
+
+        return html`
+            <nav class="nav">
+                <ul class="nav__host">
+                    ${menuItems.map(item => {
+                        const isSelected = this._activePage === item.toLowerCase();
+                        return html`
+                            <li 
+                                class="nav__items ${isSelected ? 'nav__items--active' : ''}" 
+                                @click=${this._handleNavigationClick}
+                                data-route=${item}
+                            >
+                                ${item}
+                            </li>
+                        `;
+                    })}
+                </ul>
+            </nav>
+        `;
+    }
+}
