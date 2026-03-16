c3e3e54c8ce0cd59c565b70036e4b85ea9da749a
aleksander <aprokhorenkodev@gmail.com>
2026-03-16 15:55:54 +0100

    CELLS | SERVICES | feat: create new method to get money for months

--- finance-dashboard/src/services/transaction.ts
+++ finance-dashboard/src/services/transaction.ts
@@ -16,13 +16,34 @@ export class TransactionServices {
 
     static getAllTransaction = (): Transaction[] => {
         const rawData = localStorage.getItem("transactions") || '[]';
-        return JSON.parse(rawData)
+        const desorderData: Transaction[] = JSON.parse(rawData)
+        return desorderData.sort((a, b) => {
+            const dateA = new Date(a.addedAt).getTime();
+            const dateB = new Date(b.addedAt).getTime();
+            return dateA - dateB;
+        })
     }
 
-    static getAllIconme = (filter: "ingreso" | "retirada") => {
+    static getAllmoney = (filter: TransactionType, startDate: Date, endDate: Date) => {
         return this.getAllTransaction()
-            .filter(item => item.type == filter)
+            .filter(item => {
+                const date = new Date(item.addedAt)
+                
+                return (
+                    item.type == filter &&
+                    date <= startDate &&
+                    date >= endDate
+                )
+            })
             .reduce((sum, item) => sum + item.amount, 0);
+    }
+
+    static getCurrentMonthMoney = (type: TransactionType) => {
+        const now = new Date()
+        const endDate = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate())
+        console.log(now, endDate);
+        
+        return this.getAllmoney(type, now, endDate)
     }
 
 }
