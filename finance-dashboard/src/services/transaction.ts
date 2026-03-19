bb43e71d9bce6af4f493ebf7c5429a6d99f3df88
aleksander <aprokhorenkodev@gmail.com>
2026-03-19 08:44:27 +0100

    CELLS | SERVICES | feat: add category param

--- finance-dashboard/src/services/transaction.ts
+++ finance-dashboard/src/services/transaction.ts
@@ -1,16 +1,17 @@
-import { Transaction, TransactionType } from "../types/transactions";
+import { Transaction, TransactionType, CreateTransactionDTO } from "../types/transactions";
 import { v4 } from "uuid";
+
 
 export class TransactionServices {
 
-
-    static createTransaction = (data: { [k: string]: string }): Transaction => {
+    static createTransaction = (data: CreateTransactionDTO): Transaction => {
         return {
             id: v4(),
             title: String(data.title),
             amount: Number(data.amount),
             type: data.type as TransactionType,
-            addedAt: new Date()
+            addedAt: new Date(),
+            category: data.category
         };
     }
 
@@ -28,7 +29,7 @@ export class TransactionServices {
         return this.getAllTransaction()
             .filter(item => {
                 const date = new Date(item.addedAt)
-                
+
                 return (
                     item.type == filter &&
                     date <= startDate &&
@@ -42,7 +43,7 @@ export class TransactionServices {
         const now = new Date()
         const endDate = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate())
         console.log(now, endDate);
-        
+
         return this.getAllmoney(type, now, endDate)
     }
 
