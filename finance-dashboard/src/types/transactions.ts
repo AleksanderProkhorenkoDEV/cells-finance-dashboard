32906536855e2a6831901a080d7c85d1aa4428ee
aleksander <aprokhorenkodev@gmail.com>
2026-03-19 08:43:48 +0100

    CELLS | TYPESCRIPT | feat: add type for new select and transaction

--- finance-dashboard/src/types/transactions.ts
+++ finance-dashboard/src/types/transactions.ts
@@ -4,6 +4,23 @@ export interface Transaction {
     amount: number,
     type: TransactionType,
     addedAt: Date
+    category: TransactionCategory
 }
 
 export type TransactionType = "ingreso" | "retirada"
+
+export type TransactionCategory =
+    | "comida"
+    | "transporte"
+    | "ocio"
+    | "suscripciones"
+    | "salario"
+    | "freelance";
+
+
+export interface CreateTransactionDTO{
+    title: string;
+    amount: string;
+    type: TransactionType;
+    category: TransactionCategory;
+};
